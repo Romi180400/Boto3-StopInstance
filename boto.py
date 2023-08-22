e32438efdd9704d348a2a3e0b426a9fedc6932f5
Romi1804000 <Romiomer145@gmail.com>
2023-08-22 20:33:00 +0300

    boto

--- boto.py
+++ boto.py
@@ -1,23 +1,27 @@
 import boto3
 
-ec2_client = boto3.client('ec2')
 
-INSTANCE_ID = 'i-04cc3eb848c0a7f5f'
+def stop_instance_and_create_ami(INSTANCE_ID):
+    ec2_client = boto3.client('ec2')
+    print(f"Stopping instance {INSTANCE_ID}...")
 
-ec2_client.stop_instances(InstanceIds=[INSTANCE_ID])
+    ec2_client.stop_instances(InstanceIds=[INSTANCE_ID])
 
-print(f"Stopping instance {INSTANCE_ID}...")
+    waiter = ec2_client.get_waiter('instance_stopped')
+    waiter.wait(INSTANCE_IDs=[INSTANCE_ID])
+    print(f"Instance {INSTANCE_ID} is stopped.")
 
-waiter = ec2_client.get_waiter('instance_stopped')
+    response = ec2_client.create_image(
+        InstanceId=INSTANCE_ID,
+        Name="My_Server_AMI",
+        Description="AMI created from script",
+        NoReboot=True
+    )
 
-print(f"Instance {INSTANCE_ID} is stopped.")
+    ami_id = response['ImageId']
+    print(f"AMI {ami_id} has been created.")
 
-response = ec2_client.create_image(
-    InstanceId=INSTANCE_ID,
-    Name="My_Server_AMI",
-    Description="AMI created from script",
-    NoReboot=True
-)
 
-ami_id = response['ImageId']
-print(f"AMI {ami_id} has been created.")
+if __name__ == "__main__":
+    INSTANCE_ID = 'i-04cc3eb848c0a7f5f'
+    stop_instance_and_create_ami(INSTANCE_ID)
